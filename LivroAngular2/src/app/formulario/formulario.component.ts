ec955917474c15a3ca7320639102755729362724
Fernando Nogueira <fernando.nogueira@mv.com.br>
2017-12-18 17:48:39 -0300

    Add contatos.

--- LivroAngular2/src/app/formulario/formulario.component.ts
+++ LivroAngular2/src/app/formulario/formulario.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contatos } from './contatos';
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+
+  contato = new Contatos('Thiago', '(99)99999-9999', 'email@email.com');
 
   constructor() { }
 
